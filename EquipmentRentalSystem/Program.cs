a8260a638637dc319cbd48878f987160bb052238
GustawP <gutek2003@gmail.com>
2026-03-30 23:39:47 +0200

    Add demonstration scenario in Program.cs

--- EquipmentRentalSystem/Program.cs
+++ EquipmentRentalSystem/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EquipmentRentalSystem.Models;
+using EquipmentRentalSystem.Services;
+
+var rs = new RentalService();
+var l1 = new Laptop { Name = "MacBook" };
+var s1 = new Student { FirstName = "Anna", LastName = "Bąk" };
+
+rs.AddEquipment(l1);
+rs.AddUser(s1);
+
+try {
+    rs.RentItem(s1, l1);
+    Console.WriteLine("Wypożyczono poprawnie.");
+    rs.RentItem(s1, l1);
+} catch (Exception ex) {
+    Console.WriteLine($"Błąd: {ex.Message}");
+}
+
+rs.ReturnItem(l1);
+rs.ShowAll();
