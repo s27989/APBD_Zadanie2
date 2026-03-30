d18046beeaf8283ac6e1b6ff7ab42a58e5780489
GustawP <gutek2003@gmail.com>
2026-03-30 23:35:30 +0200

    Add Rental model with due date logic

---
+++ EquipmentRentalSystem/Models/Rental.cs
@@ -0,0 +1,10 @@
+﻿namespace EquipmentRentalSystem.Models;
+
+public class Rental {
+    public Equipment Item { get; set; }
+    public User Borrower { get; set; }
+    public DateTime DateRented { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public bool IsOverdue => ReturnDate == null ? DateTime.Now > DueDate : ReturnDate > DueDate;
+}
