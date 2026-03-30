891e0000695f86f9bddf16016334657552d7face
GustawP <gutek2003@gmail.com>
2026-03-30 23:34:44 +0200

    Add Equipment and User domain models

---
+++ EquipmentRentalSystem/Models/Equipment.cs
@@ -0,0 +1,26 @@
+﻿namespace EquipmentRentalSystem.Models;
+
+public abstract class Equipment {
+    public string Id { get; set; } = Guid.NewGuid().ToString().Substring(0, 8);
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public abstract string GetInfo();
+}
+
+public class Laptop : Equipment {
+    public int Ram { get; set; }
+    public string Cpu { get; set; }
+    public override string GetInfo() => $"Laptop {Name} ({Ram}GB RAM)";
+}
+
+public class Projector : Equipment {
+    public int Lumens { get; set; }
+    public string Resolution { get; set; }
+    public override string GetInfo() => $"Projector {Name} ({Lumens} lm)";
+}
+
+public class Camera : Equipment {
+    public string SensorType { get; set; }
+    public bool 4KSupport { get; set; }
+    public override string GetInfo() => $"Camera {Name} (Sensor: {SensorType})";
+}
---
+++ EquipmentRentalSystem/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace EquipmentRentalSystem.Models;
+
+public abstract class User {
+    public string Id { get; set; } = Guid.NewGuid().ToString().Substring(0, 5);
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public abstract int MaxRentals { get; }
+}
+
+public class Student : User { public override int MaxRentals => 2; }
+public class Employee : User { public override int MaxRentals => 5; }
